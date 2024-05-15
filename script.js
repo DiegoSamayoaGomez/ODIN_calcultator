e9e217f32311903cf76b6c7978c13bd90adf08bf
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2024-05-15 16:22:10 -0600

    Calculate basic math operations and clear the display with the AC button

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-/*
-
-const btnTest = document.querySelector("#zero");
-const displayTest = document.querySelector(".operaciones");
-const btnTestOne = document.querySelector("#one");
-let sum = 0;
-let xd = btnTest.addEventListener("click", () => {
-    sum += 1;
-    console.log(sum);
-    displayTest.textContent = sum;
-    return "uwu";
-})
-let xd1 = btnTestOne.addEventListener("click", () => {
-
-    displayTest.textContent = 0;
-    return "uwu";
-})
-*/
-
 const displayTest = document.querySelector(".operaciones");
 //Numbers
 const btnZero = document.querySelector("#zero");
@@ -46,11 +26,10 @@ const btnClear = document.querySelector("#clear");
 
 const myArr = [];
 
-let a;
-let b;
+let firstNumber;
+let secondNumber;
 let sign;
 let result = 0;
-
 
 //Insert a number every time a button was pressed
 btnZero.addEventListener("click", () => {
@@ -61,7 +40,6 @@ btnOne.addEventListener("click", () => {
     myArr.push(1);
     return displayTest.textContent = myArr.join("");
 })
-
 btnTwo.addEventListener("click", () => {
     myArr.push(2);
     return displayTest.textContent = myArr.join("");
@@ -97,98 +75,221 @@ btnNine.addEventListener("click", () => {
 
 
 //Insert a sign every time a button was pressed
+
 btnDecimal.addEventListener("click", () => {
     myArr.push(".");
     return displayTest.textContent = myArr.join("");
 })
+
 btnPercentage.addEventListener("click", () => {
-    myArr.push("%");
-    return displayTest.textContent = myArr.join("");
+    if (result === 0) {
+        firstNumber = myArr.join("");
+        firstNumber = parseFloat(firstNumber);
+        console.log(firstNumber);
+        myArr.length = 0;
+        sign = "%";
+
+        return displayTest.textContent = "%";
+
+    } else {
+        myArr.length = 0;
+        firstNumber = result;
+        sign = "%";
+        return displayTest.textContent = "%";
+
+    }
 })
+
 btnInverseSign.addEventListener("click", () => {
-    myArr.push("+/-");
+    myArr.pop();
+    console.log("eliminado", myArr);
     return displayTest.textContent = myArr.join("");
 })
+
 btnDivide.addEventListener("click", () => {
-    myArr.push("/");
-    return displayTest.textContent = myArr.join("");
+    if (result === 0) {
+        firstNumber = myArr.join("");
+        firstNumber = parseFloat(firstNumber);
+        console.log(firstNumber);
+        myArr.length = 0;
+        sign = "/";
+
+        return displayTest.textContent = "/";
+
+    } else {
+        myArr.length = 0;
+        firstNumber = result;
+        sign = "/";
+        return displayTest.textContent = "/";
+    }
 })
+
 btnMultiply.addEventListener("click", () => {
-    myArr.push("*");
-    return displayTest.textContent = myArr.join("");
+    if (result === 0) {
+        firstNumber = myArr.join("");
+        firstNumber = parseFloat(firstNumber);
+        console.log(firstNumber);
+        myArr.length = 0;
+        sign = "*";
+
+        return displayTest.textContent = "*";
+
+    } else {
+        myArr.length = 0;
+        firstNumber = result;
+        sign = "*";
+
+        return displayTest.textContent = "*";
+    }
 })
+
+
 btnAdd.addEventListener("click", () => {
 
+    if (result === 0) {
+        firstNumber = myArr.join("");
+        firstNumber = parseFloat(firstNumber);
+        console.log(firstNumber);
+        myArr.length = 0;
+        sign = "+";
 
-    a = myArr.join("");
-    a = parseFloat(a);
-    console.log(a);
-    myArr.length = 0;
-    sign = "+";
+        return displayTest.textContent = "+";
 
-    return displayTest.textContent = "+";
+    } else {
+        myArr.length = 0;
+        firstNumber = result;
+        sign = "+";
+
+        return displayTest.textContent = "+";
+    }
 })
 btnRest.addEventListener("click", () => {
-    myArr.push("-");
-    return displayTest.textContent = myArr.join("");
+    if (result === 0) {
+        firstNumber = myArr.join("");
+        firstNumber = parseFloat(firstNumber);
+        console.log(firstNumber);
+        myArr.length = 0;
+        sign = "-";
+
+        return displayTest.textContent = "-";
+
+    } else {
+        myArr.length = 0;
+        firstNumber = result;
+        sign = "-";
+
+        return displayTest.textContent = "-";
+    }
 })
 
 // DO the math pls
 
 btnOperate.addEventListener("click", () => {
-    b = myArr.join("");
-    b = parseFloat(b);
-    console.log(b);
- 
-    return operate(a, b, sign);
+    secondNumber = myArr.join("");
+    secondNumber = parseFloat(secondNumber);
+    console.log("first", firstNumber);
+    console.log("second", secondNumber);
+    console.log("sign", sign);
 
+    if (sign == "%") {
+        return operate(firstNumber, 1, sign);
+    } else {
+        return operate(firstNumber, secondNumber, sign);
+
+    }
 })
 
+btnClear.addEventListener("click", () => {
+    myArr.length = 0;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    sign = undefined;
+    result = 0;
+    displayTest.textContent = "|";
+})
 
 //Basic math operations
 
-function add(a, b) {
-    return displayTest.textContent = (a + b).toFixed(2);
+function add(firstNumber, secondNumber) {
+    return displayTest.textContent = (firstNumber + secondNumber).toFixed(2);
 }
 
-function rest(a, b) {
-    return displayTest.textContent = (a - b).toFixed(2);
+function rest(firstNumber, secondNumber) {
+    return displayTest.textContent = (firstNumber - secondNumber).toFixed(2);
 }
 
-function multiply(a, b) {
-    return displayTest.textContent = (a * b).toFixed(2);
+function multiply(firstNumber, secondNumber) {
+    return displayTest.textContent = (firstNumber * secondNumber).toFixed(2);
 }
 
-function divide(a, b) {
-    return displayTest.textContent = (b > 0) ? (a / b).toFixed(2) : ("🧠n´t?");
+function divide(firstNumber, secondNumber) {
+    return displayTest.textContent = (secondNumber > 0) ? (firstNumber / secondNumber).toFixed(2) : ("🧠n´t?");
 }
+
+function percentage(firstNumber) {
+    return displayTest.textContent = firstNumber / 100;
+}
+
 
 //Get two numbers and one operator, depending on the operator, call a certain function
-function operate(a, b, sign) {
-    a = Number(a);
-    b = Number(b);
-    if ((isNaN(a)) && (isNaN(b))) {
-        displayTest.textContent = "🧠n´t? just numbers";
-    } else {
-        console.log(typeof (a), typeof (b), typeof (sign));
-        switch (sign) {
-            case "+":
-                result = (add(a, b));
-                console.log("result", result);
-                break;
-            case "-":
-                console.log(rest(a, b));
-                break;
-            case "*":
-                console.log(multiply(a, b));
-                break;
-            case "/":
-                console.log(divide(a, b));
-                break;
-            default:
-                displayTest.textContent = "Not a valid operator";
+function operate(firstNumber, secondNumber, sign) {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    try {
+
+        if ((isNaN(firstNumber)) || (isNaN(secondNumber))) {
+            displayTest.textContent = "Error";
+        } else {
+            console.log(typeof (firstNumber), typeof (secondNumber), typeof (sign));
+            switch (sign) {
+                case "+":
+                    result = (add(firstNumber, secondNumber));
+                    console.log("result", result);
+
+                    break;
+                case "-":
+                    result = (rest(firstNumber, secondNumber));
+                    console.log("result", result);
+
+                    break;
+                case "*":
+                    result = (multiply(firstNumber, secondNumber));
+                    console.log("result", result);
+
+                    break;
+                case "/":
+                    result = (divide(firstNumber, secondNumber));
+                    console.log("result", result);
+
+                    break;
+                case "%":
+                    result = (percentage(firstNumber));
+                    console.log("result", result);
+                    break;
+                //
+                default:
+                    displayTest.textContent = "Not a valid operator";
+            }
+            // After calculating the result, update the firstNumber to be the result for the next operation
+            firstNumber = result;
+            // Clear the secondNumber and sign for the next operation
+            secondNumber = undefined;
+            sign = undefined;
+            // Display the result
+            displayTest.textContent = result;
         }
+    } catch (err) {
+
+        displayTest.textContent = "Something went wrong";
+        myArr.length = 0;
+        firstNumber = undefined;
+        secondNumber = undefined;
+        sign = undefined;
+        result = 0;
+
     }
+
 }
 
 
